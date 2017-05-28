637034f24a5ecf90ecdec2099dd7a1d4fdb1feea
Esther Ng <esther.hc.ng@gmail.com>
2017-05-28 16:12:21 -0700

    Add offset

--- show_open_food_trucks.rb
+++ show_open_food_trucks.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 require 'net/http'
 require 'optparse'
 require 'date'
@@ -13,8 +15,10 @@ day = "?dayorder=#{timePST.wday}"
 sel = "&$select=applicant,location"
 order = "&$order=applicant"
 limit = "&$limit=10"
+skip_first_n = 10
+offset = "&$offset=#{skip_first_n}"
 isopen = "&$where=start24<'#{time}' AND end24>'#{time}'"
-url = URI.parse(URI.escape(base_url + day + sel + order + limit + isopen))
+url = URI.parse(URI.escape(base_url + day + sel + order + limit + offset + isopen))
 
 req = Net::HTTP::Get.new(url.to_s)
 res = Net::HTTP.start(url.host, url.port) {|http|
