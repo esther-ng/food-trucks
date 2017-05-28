44a9ec5e6903485109ef0e112c8bdd3698b1f17d
Esther Ng <esther.hc.ng@gmail.com>
2017-05-28 14:42:03 -0700

    Add formatador for table print

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source "http://rubygems.org"
+
+gem 'formatador', '~> 0.2.5'
--- show_open_food_trucks.rb
+++ show_open_food_trucks.rb
@@ -1,6 +1,8 @@
 require 'net/http'
 require 'optparse'
 require 'date'
+require 'json'
+require 'formatador'
 
 TZ = "-07:00"
 base_url = 'http://data.sfgov.org/resource/bbb8-hzi6.json'
@@ -18,4 +20,10 @@ req = Net::HTTP::Get.new(url.to_s)
 res = Net::HTTP.start(url.host, url.port) {|http|
   http.request(req)
 }
-puts res.body
+
+data = JSON.parse(res.body)
+data.each do |f|
+  f["NAME"] = f.delete("applicant")
+  f["ADDRESS"] = f.delete("location")
+end
+Formatador.display_table(data, ["NAME", "ADDRESS"])
