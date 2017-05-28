84a00f651f564fdba04910e895664d0f229a259a
Esther Ng <esther.hc.ng@gmail.com>
2017-05-28 13:32:25 -0700

    Basic filtering

--- show_open_food_trucks.rb
+++ show_open_food_trucks.rb
@@ -1,6 +1,19 @@
 require 'net/http'
+require 'optparse'
+require 'date'
 
-url = URI.parse('http://data.sfgov.org/resource/bbb8-hzi6.json')
+TZ = "-07:00"
+base_url = 'http://data.sfgov.org/resource/bbb8-hzi6.json'
+now = Time.now
+timePST = now.getlocal(TZ)
+time = timePST.strftime("%R")
+day = "?dayorder=#{timePST.wday}"
+sel = "&$select=applicant,location"
+order = "&$order=applicant"
+limit = "&$limit=10"
+isopen = "&$where=start24<'#{time}' AND end24>'#{time}'"
+url = URI.parse(URI.escape(base_url + day + sel + order + limit + isopen))
+
 req = Net::HTTP::Get.new(url.to_s)
 res = Net::HTTP.start(url.host, url.port) {|http|
   http.request(req)
