6a0c2178fb2cc496aa5bbc940d7e597d2fff0950
Esther Ng <esther.hc.ng@gmail.com>
2017-05-30 20:17:15 -0700

    Show next 10 or exit based on user input

--- show_open_food_trucks.rb
+++ show_open_food_trucks.rb
@@ -6,28 +6,61 @@ require 'date'
 require 'json'
 require 'formatador'
 
-TZ = "-07:00"
-base_url = 'http://data.sfgov.org/resource/bbb8-hzi6.json'
-now = Time.now
-timePST = now.getlocal(TZ)
-time = timePST.strftime("%R")
-day = "?dayorder=#{timePST.wday}"
-sel = "&$select=applicant,location"
-order = "&$order=applicant"
-limit = "&$limit=10"
-skip_first_n = 10
-offset = "&$offset=#{skip_first_n}"
-isopen = "&$where=start24<'#{time}' AND end24>'#{time}'"
-url = URI.parse(URI.escape(base_url + day + sel + order + limit + offset + isopen))
 
-req = Net::HTTP::Get.new(url.to_s)
-res = Net::HTTP.start(url.host, url.port) {|http|
-  http.request(req)
-}
+class API
+  TZ = "-07:00"
+  BASE_URL = "http://data.sfgov.org/resource/bbb8-hzi6.json"
+  LIMIT = 10
+  SELECTED_COLUMNS = "applicant, location"
 
-data = JSON.parse(res.body)
-data.each do |f|
-  f["NAME"] = f.delete("applicant")
-  f["ADDRESS"] = f.delete("location")
+  def initialize
+    @skip_first_n = 0
+  end
+
+  def url
+    timePST = Time.now.getlocal(TZ)
+    time = timePST.strftime("%R")
+    day = "?dayorder=#{timePST.wday}"
+    sel = "&$select=#{SELECTED_COLUMNS}"
+    order = "&$order=applicant"
+    limit = "&$limit=#{LIMIT}"
+    offset = "&$offset=#{@skip_first_n}"
+    isopen = "&$where=start24<'#{time}' AND end24>'#{time}'"
+    url = URI.parse(URI.escape(BASE_URL + day + sel + order + limit + offset + isopen))
+    url
+  end
+
+  def get_open
+    req = Net::HTTP::Get.new(url.to_s)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    data = JSON.parse(res.body)
+    data.each do |f|
+      f["NAME"] = f.delete("applicant")
+      f["ADDRESS"] = f.delete("location")
+    end
+    data
+  end
+
+  def show_open
+    Formatador.display_table(get_open, ["NAME", "ADDRESS"])
+    @skip_first_n += LIMIT
+  end
 end
-Formatador.display_table(data, ["NAME", "ADDRESS"])
+
+foodtrucks = API.new
+foodtrucks.show_open
+show_next = ""
+while true
+  puts "Show next 10? Y/N: "
+  show_next = gets.chomp.downcase
+  if show_next == "y"
+    foodtrucks.show_open
+  elsif show_next == "n"
+    exit(0)
+  else
+    puts "Sorry, that is not a valid option. Show next 10? Y/N: "
+    show_next = gets.chomp.downcase
+  end
+end
